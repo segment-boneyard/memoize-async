3ddcb228f5910c62d5bb868cf5eadc84630f6afc
Calvin French-Owen <calvin@calv.info>
2013-10-22 16:51:17 -0700

    bugfixes

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 
-var nextTick = require('next-tick')
-  , slice    = Array.prototype.slice;
+var nextTick = require('next-tick');
 
+
+/**
+ * Utility methods
+ */
+
+var slice = Array.prototype.slice;
+var has = Object.hasOwnProperty;
+
+
+/**
+ * Module exports
+ */
 
 module.exports = memoize;
 
@@ -12,24 +23,25 @@ module.exports = memoize;
  * @param  {Function} keyFn     how to transform the arguments into a key
  * @return {Function} memoized  the memoized function
  */
+
 function memoize (fn, keyFn) {
   keyFn || (keyFn = function (key) { return key; });
 
-  var cached    = {}
-    , callbacks = {};
+  var cached = {};
+  var callbacks = {};
 
   return function () {
+    var args = initial(arguments);
+    var cb = last(arguments);
+    var key = keyFn.apply(keyFn, args);
 
-    var args = initial(arguments)
-      , cb   = last(arguments)
-      , key  = keyFn(args);
-
-    if (key in cached) {
+    if (has.call(cached, key)) {
       return nextTick(function () { cb.apply(cb, toRes(null, cached[key])); });
     }
 
-    // replace the old callback function
-    callbacks[key] = callbacks[key] || [];
+    if (has.call(callbacks, key)) return callbacks[key].push(cb);
+
+    callbacks[key] = [];
     callbacks[key].push(cb);
 
     args.push(onFinish);
@@ -37,7 +49,6 @@ function memoize (fn, keyFn) {
 
     function onFinish (err) {
       if (!err) cached[key] = rest(arguments); // save our res only on success
-
       respond(callbacks[key], arguments); // callback
       delete callbacks[key];
     }
@@ -50,12 +61,13 @@ function memoize (fn, keyFn) {
  * @param  {Array} callbacks
  * @param  {Array} args
  */
+
 function respond (callbacks, args) {
   if (!callbacks) return;
 
-  for (var i = 0; i < callbacks[key].length; i++) {
+  for (var i = 0; i < callbacks.length; i++) {
     var callback = callbacks[i];
-    callback[i].apply(callback, args);
+    callback.apply(callback, args);
   }
 }
 
@@ -63,6 +75,7 @@ function respond (callbacks, args) {
 /**
  * Return all but the last argument from the array
  */
+
 function initial (args) {
   return slice.apply(args, [0, -1]);
 }
@@ -71,6 +84,7 @@ function initial (args) {
 /**
  * Return all but the first argument from the array
  */
+
 function rest (args) {
   return slice.apply(args, [1]);
 }
@@ -79,6 +93,7 @@ function rest (args) {
 /**
  * Return only the last argument from the array
  */
+
 function last (args) {
   return args[args.length - 1];
 }
@@ -87,6 +102,7 @@ function last (args) {
 /**
  * Turn an error and arguments object into a proper callback array
  */
+
 function toRes (err, args) {
-  return [err] + args;
+  return [err].concat(args);
 }
--- test/index.js
+++ test/index.js
@@ -2,26 +2,36 @@
 var memoize = require('..')
   , assert  = require('assert');
 
-var count = [];
 
-function async (index, cb) {
-  setTimeout(function () {
-    count[index] = count[index] || index;
-    count[index] += 1;
-    cb(null, count[index]);
-  }, 100);
-}
+describe('memoize-async', function () {
 
-var memoized = memoize(async);
+  it('should only call back the same value once', function (done) {
+    var count = [];
 
-memoized(0, function (err, result) {
-  assert(result === 1);
+    function async (index, cb) {
+      setTimeout(function () {
+        count[index] = count[index] || index;
+        count[index] += 1;
+        cb(null, count[index]);
+      }, 10);
+    }
+
+    var memoized = memoize(async);
+    memoized(0, function (err, result) { assert(result === 1); });
+    memoized(0, function (err, result) { assert(result === 1); });
+    memoized(1, function (err, result) { assert(result === 2); });
+    setTimeout(done, 40);
+  });
+
+  it('should use a key function', function (done) {
+    function callback (index, cb) { cb(null, index + 5); }
+    var memoized = memoize(callback, function (i) { return i % 3; });
+    memoized(0, function (err, result) { assert(result === 5); });
+    memoized(3, function (err, result) { assert(result === 5); });
+    memoized(10, function (err, result) { assert(result === 15); });
+    memoized(1, function (err, result) { assert(result === 15); });
+    setTimeout(done, 10);
+  });
 });
 
-memoized(0, function (err, result) {
-  assert(result === 1);
-});
 
-memoized(1, function (err, result) {
-  assert(result === 2);
-});
